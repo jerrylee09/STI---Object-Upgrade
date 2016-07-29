02480316d264644bc8e4ca5c308eb631fc41b327
Jerry Lee <jerry.lee09@yahoo.com>
2016-07-29 08:47:56 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var arrayAtticus = ["Atticus", "2405", "47000", 3];
+var arrayJem = ["Jem", "62347", "63500", 4];
+var arrayBoo = ["Boo", "11435", "54000", 3];
+var arrayScout = ["Scout", "6243", "74750", 5];
+
+
+function Person() {
+
+
+}
